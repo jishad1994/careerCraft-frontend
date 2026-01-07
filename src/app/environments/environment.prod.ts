02896bfb98b9ec5a3b0533b6da3df6e0e4a6b80f
jishad kolapurath <jishadkolapurath@gmail.com>
2026-01-07 14:59:34 +0530

    Restore environments locally and untrack environment files

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+// environment.prod.ts (production)
+export const environment = {
+  production: true,
+  apiUrl: 'https://your-domain.com/api',
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,13 @@
+// environment.ts (development)
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  authBaseUrl:'/api/auth',
+  userBaseUrl:'/api/user',
+  companyBaseUrl:'/api/company',
+  adminBaseUrl:'/api/admin',
+  logUrl: 'assets/images/naukriLogo.svg',
+
+  GOOGLE_CLIENT_ID:
+    '837953702796-lmr8bdkm366etmrbam57ejsog2m5onbq.apps.googleusercontent.com',
+};
