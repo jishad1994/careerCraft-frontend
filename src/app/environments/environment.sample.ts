e0860ab6b462dfa06aee512d51913aa32df4b5e5
jishad kolapurath <jishadkolapurath@gmail.com>
2026-01-07 17:41:56 +0530

    add sample environment file

---
+++ src/app/environments/environment.sample.ts
@@ -0,0 +1,12 @@
+const sampleEnvironment = {
+  production: true,
+
+  // #urls
+  apiUrl: 'https://your-domain.com',
+  authBaseUrl: '',
+  userBaseUrl: '',
+  companyBaseUrl: '',
+  adminBaseUrl: '',
+  logUrl: '',
+  GOOGLE_CLIENT_ID: '',
+};
