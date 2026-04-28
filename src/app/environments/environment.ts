0e36d402a969890056247186e82f0c3b37718085
jishad kolapurath <jishadkolapurath@gmail.com>
2026-04-28 19:11:10 +0530

    add env files to git

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,20 @@
+// environment.ts (development)
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:4000',
+  socketUrl: 'http://localhost:4000',
+  authBaseUrl: '/api/auth',
+  userBaseUrl: '/api/user',
+  companyBaseUrl: '/api/company',
+  adminBaseUrl: '/api/admin',
+  skillManagementBaseUrl: '/api/skills',
+  logUrl: 'assets/images/naukriLogo.svg',
+
+  GOOGLE_CLIENT_ID:
+    '837953702796-lmr8bdkm366etmrbam57ejsog2m5onbq.apps.googleusercontent.com',
+
+  PUBLIC_LOCATION_API_URL: 'https://nominatim.openstreetmap.org',
+
+  STRIPE_PUBLISHABLE_KEY:
+    'pk_test_51T4H1SAXVRJhzuMJrhhQrAMF6H0bOMcrvW05YZMTjPiqhTqLy3XHwRJPqVb87alWIrgytd1h3hpGtKV70Gp8YMuU00LM7yWLqg',
+};
