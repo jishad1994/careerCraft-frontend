3c8088ebb17dcfdbdd54c595f68cc9b6e623f9f6
jishad kolapurath <jishadkolapurath@gmail.com>
2026-05-04 16:49:33 +0530

    change env production values

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,18 @@
+// environment.prod.ts (production)
+export const environment = {
+  production: true,
+  apiUrl: 'https://13.48.193.168:4000',
+  socketUrl: 'https://13.48.193.168:4000',
+  authBaseUrl:'/api/auth',
+  userBaseUrl:'/api/user',
+  companyBaseUrl:'/api/company',
+  adminBaseUrl:'/api/admin',
+  logUrl: 'assets/images/naukriLogo.svg',
+
+  GOOGLE_CLIENT_ID:
+    '837953702796-lmr8bdkm366etmrbam57ejsog2m5onbq.apps.googleusercontent.com',
+
+    PUBLIC_LOCATION_API_URL: 'https://nominatim.openstreetmap.org',
+     STRIPE_PUBLISHABLE_KEY:
+    'pk_test_51T4H1SAXVRJhzuMJrhhQrAMF6H0bOMcrvW05YZMTjPiqhTqLy3XHwRJPqVb87alWIrgytd1h3hpGtKV70Gp8YMuU00LM7yWLqg',
+};
